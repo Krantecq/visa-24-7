51b915b59a6043c0540d0b2796d775e5fbd1283e
laxit <laxitnaharcp1811@gmail.com>
2023-10-14 10:01:20 +0530

    analytics page

--- src/app/components/MerchantAnalytics.tsx
+++ src/app/components/MerchantAnalytics.tsx
@@ -3,7 +3,12 @@ import icon1 from '../../_metronic/assets/card/1.png'
 import customer from '../../_metronic/assets/card/customer.png'
 import merchant from '../../_metronic/assets/card/merchant.png'
 
-export function MerchantAnaltytics() {
+type Props = {
+  dashboardData: any
+}
+
+
+export function MerchantAnaltytics({dashboardData}) {
     return (
         <>
    <div className='row g-5 g-xl-10'>
@@ -11,41 +16,41 @@ export function MerchantAnaltytics() {
       <div className=' col-md-6 col-lg-6 col-xl-6 col-xxl-3'>
         <HomeMainCard
           className='mb-5 mb-xl-10'
-          description='API Merchants'
+          description='Wallet Balance'
           color='#F0F0F0'
           icon={icon1}
           textColor='#071437'
-          count='2'
+          count={dashboardData.wallet_balance}
         />
       </div>
       <div className=' col-md-6 col-lg-6 col-xl-6 col-xxl-3'>
         <HomeMainCard
           className='mb-5 mb-xl-10'
-          description='Individual Customers'
+          description='Total Visa'
           color='#071537'
           icon={customer}
           textColor='#ffff'
-          count={'12'}
+          count={dashboardData.total_visa}
         />
       </div>
       <div className=' col-md-6 col-lg-6 col-xl-6 col-xxl-3'>
         <HomeMainCard
           className='mb-5 mb-xl-10'
-          description='Registered Merchants'
+          description='Approved Visa'
           color='#FFC703'
           icon={merchant}
           textColor='#FFFF'
-          count={'20'}
+          count={dashboardData.approved_visa}
         />
       </div>
       <div className=' col-md-6 col-lg-6 col-xl-6 col-xxl-3'>
         <HomeMainCard
           className='mb-5 mb-xl-10'
-          description='No. of Visa Rejected'
+          description='No. of Visa In-Process'
           color='#7239EB'
           icon={icon1}
           textColor='#FFFF'
-          count={'76'}
+          count={dashboardData.in_process_visa}
         />
       </div>
     </div>
@@ -53,35 +58,24 @@ export function MerchantAnaltytics() {
       <div className=' col-md-6 col-lg-6 col-xl-6 col-xxl-3 '>
         <HomeMainCard
           className='mb-5 mb-xl-10'
-          description='No. of Visa In-Process'
+          description='No. of Visa In Waiting'
           color='#F0F0F0'
           icon={icon1}
           textColor='#071437'
-          count={'72'}
+          count={dashboardData.waiting_visa}
         />
       </div>
       <div className=' col-md-6 col-lg-6 col-xl-6 col-xxl-3 '>
         <HomeMainCard
           className='mb-5 mb-xl-10'
-          description='No. of Visa Waiting for Approval'
+          description='No. of Visa Not Issued'
           color='#071537'
           icon={customer}
           textColor='#ffff'
-          count={'21'}
-        />
-      </div>
-      <div className=' col-md-6 col-lg-6 col-xl-6 col-xxl-3 '>
-        <HomeMainCard
-          className='mb-5 mb-xl-10'
-          description='Atlys Wallet Balance'
-          color='#FFC703'
-          icon={merchant}
-          textColor='#FFFF'
-          count={'21'}
+          count={dashboardData.not_issued_visa}
         />
       </div>
     </div>
         </>
     )
-
 }
