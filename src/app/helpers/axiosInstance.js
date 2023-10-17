1523dc450865939992f9dc4076b08b73c278804e
Amnajeet12 <amanjeetsingh739@gmail.com>
2023-10-18 01:42:48 +0530

    baseurl

--- src/app/helpers/axiosInstance.js
+++ src/app/helpers/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Instance = axios.create({
-  baseURL: 'http://69.55.55.245:5003',
+  baseURL: 'http://64.227.160.58:5003',
   credentials: "include",
   withCredentials: true,
 });
