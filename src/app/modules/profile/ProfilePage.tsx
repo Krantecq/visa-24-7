71beb9f965262063fb0bfc358b6b756805651021
laxit <laxitnaharcp1811@gmail.com>
2023-10-20 10:26:44 +0530

    profile function

--- src/app/modules/profile/ProfilePage.tsx
+++ src/app/modules/profile/ProfilePage.tsx
@@ -6,6 +6,33 @@ import {Campaigns} from './components/Campaigns'
 import {Documents} from './components/Documents'
 import {Connections} from './components/Connections'
 import {ProfileHeader} from './ProfileHeader'
+import axiosInstance from '../../helpers/axiosInstance'
+import Cookies from 'js-cookie'
+
+const fetchData = async () => {
+  try {
+    const user_id = Cookies.get('user_id')
+
+    // Make a POST request to your API endpoint
+    axiosInstance.post('/backend/fetch_super_admin',{
+      id:user_id
+    })
+      .then((response) => {
+        console.log(response.data.data)
+        const responseData = response.data.data;
+
+        // Update the formData state with the fetched data
+       
+      })
+      .catch((error) => {
+        console.error('Error fetching Atlys data:', error);
+      });
+
+
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
 
 const profileBreadCrumbs: Array<PageLink> = [
   {
