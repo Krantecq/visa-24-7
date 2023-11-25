2bf107bb13504d7cc7e531a8064f7a0e0165a134
laxit <laxitnaharcp1811@gmail.com>
2023-11-25 18:44:29 +0530

    revenue

--- src/app/components/RevenueTable.tsx
+++ src/app/components/RevenueTable.tsx
@@ -205,7 +205,7 @@ const RevenueTable: React.FC<Props> = ({ className, title, data, loading }) => {
                   <th className='min-w-80px text-center'>Merchant ID</th>
                   <th className='min-w-80px text-center'>Provider</th>
                   
-                  <th className='min-w-80px text-center'>Visa Cost</th>
+                  {/* <th className='min-w-80px text-center'>Visa Cost</th> */}
                   <th className='min-w-80px text-center'>Paid</th>
                   <th className='min-w-80px text-center'>Recieved</th>
                   <th className='min-w-80px text-center'>Margin</th>
@@ -240,7 +240,7 @@ const RevenueTable: React.FC<Props> = ({ className, title, data, loading }) => {
                       {/* Location 1 */}
                       <a href='#' className='text-dark fw-bold text-hover-primary d-block fs-6' 
                           title={`${row.name} 
-${row.application_no}`}>
+${row.id}`}>
                         {row.name}
                       </a>
                     </td>
@@ -248,26 +248,20 @@ ${row.application_no}`}>
                     <td className='text-center'>
                       {/* Location 1 */}
                       <a href='#' className='text-dark fw-bold text-hover-primary d-block fs-6'>
-                        {row.customer_type}{/* Margin */}
-                      </a>
-                    </td>
-
-                    <td className='text-center'>
-                      {/* Location 1 */}
-                      <a href='#' className='text-dark fw-bold text-hover-primary d-block fs-6'>
-                        {row.revenue}{/* Time of Transaction */}
+                        Atlys{/* Margin */}
                       </a>
                     </td>
                     <td className='text-center'>
                       {/* Location 1 */}
                       <a href='#' className='text-dark fw-bold text-hover-primary d-block fs-6'>
-                        {row.revenue}{/* Time of Transaction */}
+                        {row.paid}{/* Time of Transaction */}
                       </a>
                     </td>
+                    
                     <td className='text-center'>
                       {/* Location 1 */}
                       <a href='#' className='text-dark fw-bold text-hover-primary d-block fs-6'>
-                        {row.revenue}{/* Time of Transaction */}
+                        {row.receive}{/* Time of Transaction */}
                       </a>
                     </td>
                     <td className='text-center'>
